1a0822feb41e4d2aa4d980923a002f1cb4402d78
Henrik Cullen <hccullen@users.noreply.github.com>
2025-07-16 15:49:27 +0200

    update again

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { DictationConfig } from './types.js';
 
-export const LANGUAGES_SUPPORTED = ['en', 'en-GB', 'da', 'de', 'fr', 'de-CH', 'sv', 'es', 'it', 'nl', 'no', 'pt'];
+export const LANGUAGES_SUPPORTED = ['en', 'en-GB', 'da', 'de', 'fr', 'sv' ];
 export const DEFAULT_DICTATION_CONFIG: DictationConfig = {
   primaryLanguage: 'en',
   interimResults: true,
